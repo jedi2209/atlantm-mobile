b94a29cdf1de1a35434bf979576d0cfdb94a0933
Nickolay Ilchenko <tavria@yandex-team.ru>
2017-08-17 02:33:58 +0300

    Линковка google maps

--- src/contacts/map/containers/MapScreen.js
+++ src/contacts/map/containers/MapScreen.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   Image,
   View,
+  Dimensions,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import {
@@ -11,22 +12,35 @@ import {
   Text,
 } from 'native-base';
 
+// Maps
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+// Redux
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+// Helpers
 import styleConst from '../../../core/style-const';
 import { scale } from '../../../utils/scale';
 import styleHeader from '../../../core/components/Header/style';
 
 const styles = StyleSheet.create({
   content: {
-    backgroundColor: styleConst.color.content,
+    backgroundColor: styleConst.color.bg,
   },
 });
 
-const mapStateToProps = () => {
+const mapStateToProps = ({ dealer }) => {
   return {
-
+    dealerSelected: dealer.selected,
   };
 };
 
@@ -46,7 +60,7 @@ class MapScreen extends Component {
   render() {
     return (
       <Container>
-        <Content>
+        <Content style={styles.content} >
           <Text>Карта</Text>
         </Content>
       </Container>
