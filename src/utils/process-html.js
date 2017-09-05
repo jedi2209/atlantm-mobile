c3c79e76e4997799bf8b4af7f6e1b05fb42e4f55
Nickolay Ilchenko <tavria@yandex-team.ru>
2017-09-06 01:09:45 +0300

    Поправил высота блока с html после тестирования на реальном девайсе

--- src/utils/process-html.js
+++ src/utils/process-html.js
@@ -1,21 +1,38 @@
 import styleConst from '../core/style-const';
 
-const htmlStyles = `
-  <style>
+const processHtml = (text, width, height) => {
+  const result = text.replace(/<a[^>]*>([^<]*)<\/a>/ig, (i1, i2) => `<p>${i2}</p>`);
+
+  return `
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Акция Atlant-m</title>
+
+    </head>
+    <body>
+    <style>
     html, body {
+      width: ${width};
+      max-width: 100%;
       font-size: 15px;
       background-color: ${styleConst.color.bg};
       font-family: 'Helvetica Neue';
+      padding: 0;
+      margin: 0;
+      overflow: 'scroll';
     }
     table {
       font-size: 13px;
     }
   </style>
-`;
-
-const processHtml = (text) => {
-  const result = text.replace(/<a[^>]*>([^<]*)<\/a>/ig, (i1, i2) => `<p>${i2}</p>`);
-  return htmlStyles + result;
+      ${result}
+    </body>
+    </html>
+  `;
 };
 
 export default processHtml;
