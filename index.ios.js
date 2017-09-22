a270e381d95c18cf57153361171ecaf95538ca56
Nickolay Ilchenko <tavria@yandex-team.ru>
2017-09-22 15:33:04 +0300

    Android build 27, ios 5, senry integration
    На iOS пока без sentry

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,11 @@ import { AppRegistry } from 'react-native';
 // component
 import { Wrapper } from './src/core/containers/Wrapper';
 
+import { Sentry } from 'react-native-sentry';
+
+Sentry.config("https://2e35f2a2455b4a3d97a1687270845d33:4df609d533fd4ce3be4fa721e6583c87@sentry.io/219899").install();
+
+
 class AtlantmIOSApplication extends Component {
   render() {
     return <Wrapper />;
