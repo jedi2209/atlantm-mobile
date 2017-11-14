12170c7eb6b567720f8bff5a73151a176a198546
Nickolay Ilchenko <tavria@yandex-team.ru>
2017-11-15 00:14:37 +0300

    Верстка результатов поиска ТВА

--- src/profile/components/ListItemHeader.js
+++ src/profile/components/ListItemHeader.js
@@ -1,24 +1,54 @@
 import React, { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 // components
-import { ListItem } from 'native-base';
+import { ListItem, Icon } from 'native-base';
+
+// helpers
+import styleConst from '../../core/style-const';
+
+const styles = StyleSheet.create({
+  icon: {
+    color: styleConst.color.systemBlue,
+    marginRight: 10,
+    marginTop: 1,
+  },
+});
 
 export default class ListItemHeader extends PureComponent {
   static propTypes = {
     text: PropTypes.string,
+    radio: PropTypes.bool,
+    radioSelected: PropTypes.bool,
+    onPress: PropTypes.func,
   }
 
   static defaultProps = {
+    textStyle: {},
     text: '',
+    radio: false,
+    radioSelected: false,
   }
 
   render() {
+    const { text, radio, radioSelected, textStyle, onPress } = this.props;
+
     return (
       <View>
         <ListItem itemHeader>
-          <Text>{this.props.text}</Text>
+          {
+              radio ?
+                (
+                  <Icon
+                    name={radioSelected ? 'md-radio-button-on' : 'md-radio-button-off'}
+                    style={[
+                      styles.icon,
+                    ]}
+                  />
+                ) : null
+            }
+          <Text style={textStyle}>{this.props.text}</Text>
         </ListItem>
       </View>
     );
--- src/tva/actions.js
+++ src/tva/actions.js
@@ -4,19 +4,47 @@ import {
   TVA__REQUEST,
   TVA__SUCCESS,
   TVA__FAIL,
-
   TVA_CAR_NUMBER__FILL,
+
+  TVA_SEND_MESSAGE__REQUEST,
+  TVA_SEND_MESSAGE__SUCCESS,
+  TVA_SEND_MESSAGE__FAIL,
+  TVA_MESSAGE__FILL,
+
+  TVA_ORDER_ID__SET,
 } from './actionTypes';
 
 export const carNumberFill = (carNumber) => {
-  // if (carNumber && carNumber.length <= 5) {
-  //   carNumber = carNumber.trim();
-  // }
+  if (carNumber && carNumber.length <= 5) {
+    carNumber = carNumber.trim();
+  }
 
   return dispatch => {
     dispatch({
       type: TVA_CAR_NUMBER__FILL,
       payload: carNumber,
+    });
+  };
+};
+
+export const actionTvaMessageFill = (message) => {
+  if (message && message.length <= 3) {
+    message = message.trim();
+  }
+
+  return dispatch => {
+    dispatch({
+      type: TVA_MESSAGE__FILL,
+      payload: message,
+    });
+  };
+};
+
+export const actionSetActiveTvaOrderId = (orderId) => {
+  return dispatch => {
+    dispatch({
+      type: TVA_ORDER_ID__SET,
+      payload: orderId,
     });
   };
 };
@@ -54,6 +82,41 @@ export const actionFetchTva = (props) => {
       .catch(error => {
         return dispatch({
           type: TVA__FAIL,
+          payload: {
+            error: error.message,
+            code: error.code,
+          },
+        });
+      });
+  };
+};
+
+export const actionTvaMessageSend = (props) => {
+  return dispatch => {
+    dispatch({
+      type: TVA_SEND_MESSAGE__REQUEST,
+      payload: { ...props },
+    });
+
+    return API.tvaMessageSend(props)
+      .then(res => {
+        const { error, status } = res;
+
+        if (status !== 'success') {
+          return dispatch({
+            type: TVA_SEND_MESSAGE__FAIL,
+            payload: {
+              code: error.code,
+              message: error.message,
+            },
+          });
+        }
+
+        return dispatch({ type: TVA_SEND_MESSAGE__SUCCESS });
+      })
+      .catch(error => {
+        return dispatch({
+          type: TVA_SEND_MESSAGE__FAIL,
           payload: {
             error: error.message,
             code: error.code,
