a5d1c2bd52c72ede9f344fc6c483dddb86814fde
Nickolay Ilchenko <tavria@yandex-team.ru>
2017-12-03 02:10:16 +0300

    Добавил на экран отзыва – ответ автоцентра

--- src/core/style-const.js
+++ src/core/style-const.js
@@ -27,6 +27,7 @@ export default {
   ui: {
     borderWidth: 1 / PixelRatio.getPixelSizeForLayoutSize(1),
     horizontalGap: 11,
+    horizontalGapInList: 14,
     verticalGap: 22,
     letterSpacing: -0.3,
     footerHeight: 50,
