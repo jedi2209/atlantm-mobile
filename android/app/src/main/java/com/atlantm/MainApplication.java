4c95b2ede2f17892b9a46eae687f66af496b49cb
Nickolay Ilchenko <tavria@yandex-team.ru>
2018-01-21 18:12:25 +0300

    Подключить библиотеку для загрузки фотографий

--- android/app/src/main/java/com/atlantm/MainApplication.java
+++ android/app/src/main/java/com/atlantm/MainApplication.java
@@ -3,6 +3,7 @@ package com.atlantm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import io.sentry.RNSentryPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PickerPackage(),
             new FIRMessagingPackage(),
             new RNSentryPackage(MainApplication.this),
           new SplashScreenReactPackage(),
