76774e4c5f113935e455ce9b7d32f4d6c37af0d0
Nickolay Ilchenko <tavria@yandex-team.ru>
2018-10-08 20:50:46 +0300

    Восстановил иконки и splashscreen

--- ios/atlantm/AppDelegate.m
+++ ios/atlantm/AppDelegate.m
@@ -10,6 +10,9 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+// modules
+#import "RNSplashScreen.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -29,6 +32,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
