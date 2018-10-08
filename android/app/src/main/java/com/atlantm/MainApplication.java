3af0fad3644ef67010380cc9c02fdc874a52de7c
Nickolay Ilchenko <tavria@yandex-team.ru>
2018-10-09 02:02:34 +0300

    Подключили amplitude на обе платформы

--- android/app/src/main/java/com/atlantm/MainApplication.java
+++ android/app/src/main/java/com/atlantm/MainApplication.java
@@ -3,6 +3,7 @@ package com.atlantm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sudoplz.reactnativeamplitudeanalytics.RNAmplitudeSDKPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+              new RNAmplitudeSDKPackage(MainApplication.this),
               new RNFetchBlobPackage(),
               new PickerPackage(),
               new RNDeviceInfo(),
--- src/utils/amplitude-analytics.js
+++ src/utils/amplitude-analytics.js
@@ -1,10 +1,10 @@
-import RNAmplitute from 'react-native-amplitude-analytics';
-import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
+import RNAmplitude from 'react-native-amplitude-analytics';
+// import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
 export default class Amplitude {
   static getInstance() {
     if (!this.instance) {
-      this.instance = new RNAmplitute('XXXX');
+      this.instance = new RNAmplitude('XXXX');
     }
     return this.instance;
   }
@@ -20,7 +20,7 @@ export default class Amplitude {
   static logEvent(category, action, params) {
     if (!__DEV__) {
       this.getInstance().logEvent(`${category}:${action}`, params);
-      this.getGATracker().trackEvent(category, action, params);
+      // this.getGATracker().trackEvent(category, action, params);
     }
   }
 }
