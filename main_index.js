12580eab32ffa35e9e0461003f42697135b29f21
Nickolay Ilchenko <tavria@yandex-team.ru>
2018-10-08 19:36:57 +0300

    Добавил чистый react-native@0.57

---
+++ main_index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NativeModules, AppRegistry, YellowBox } from 'react-native';
+import { Sentry } from 'react-native-sentry';
+
+YellowBox.ignoreWarnings([
+  'Remote debugger',
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Module AIRGoogleMapManager requires main queue setup since it overrides',
+  'Module RCTImageLoader requires main queue setup since it overrides',
+  'Module RNFetchBlob requires main queue setup since it overrides',
+  'Module ImageCropPicker requires main queue setup since it overrides',
+]);
+
+// For RN < 0.43
+if (__DEV__) {
+  NativeModules.DevSettings.setIsDebuggingRemotely(true);
+}
+
+import Wrapper from './src/core/containers/Wrapper';
+
+Sentry
+  .config('https://XXXX:4df609d533fd4ce3be4fa721e6583c87@sentry.io/219899')
+  .install();
+
+const AtlantmApplication = () => <Wrapper />;
+
+AppRegistry.registerComponent('atlantm', () => AtlantmApplication);
