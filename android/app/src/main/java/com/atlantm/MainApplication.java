8e6ca72473fb18be24389890fc05f2b6f1d93c75
Nickolay Ilchenko <tavria@yandex-team.ru>
2018-10-09 00:52:42 +0300

    Подключили rn-fetch-blob на обе платформы

--- android/app/src/main/java/com/atlantm/MainApplication.java
+++ android/app/src/main/java/com/atlantm/MainApplication.java
@@ -3,6 +3,7 @@ package com.atlantm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+              new RNFetchBlobPackage(),
               new PickerPackage(),
               new RNDeviceInfo(),
               new MapsPackage(),
