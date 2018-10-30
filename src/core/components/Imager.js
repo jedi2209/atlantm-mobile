64e0a9446c906ba53dc07e34efacc92a02288f65
Zavarka Team Support <support@zteam.ru>
2018-10-30 19:33:06 +0300

    Вернул лоадеры для изображений

--- src/core/components/Imager.js
+++ src/core/components/Imager.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 // components
-import { Image } from 'react-native';
+import { Image, View, ActivityIndicator } from 'react-native';
 
 export default class Imager extends PureComponent {
   static propTypes = {
@@ -16,7 +16,12 @@ export default class Imager extends PureComponent {
 
     this.state = {
       imagePath: this.props.source.uri,
+      animatingLoader: true
     };
+  }
+
+  componentDidMount(){
+      this.state.animating = false;
   }
 
   getImageCacheManager = () => {
@@ -24,6 +29,7 @@ export default class Imager extends PureComponent {
       this.imageCacheManager = ImageCacheManager();
     }
 
+    this.setState({animatingLoader: false});
     return this.imageCacheManager;
   }
 
@@ -31,10 +37,26 @@ export default class Imager extends PureComponent {
     console.log('== Imager ==');
 
     return (
-      <Image
-        {...this.props}
-        source={{ uri: this.state.imagePath }}
-      >{this.props.children}</Image>
+    <View>
+      <ActivityIndicator
+          animating={this.state.animatingLoader}
+          style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+          justifyContent: 'center',
+          alignItems: 'center'
+      }} />
+      <Image {...this.props}
+             source={{ uri: this.state.imagePath }}
+             // onLoadStart={() => { console.log('Image on load start'); }}
+             // onLoad={() => { this.setState({animatingLoader: false}); console.log('Image on load'); }}
+             onLoadEnd={() => { this.setState({animatingLoader: false}); console.log('Image on load end'); }}>
+          {this.props.children}
+      </Image>
+    </View>
     );
   }
 }
