6186d0469b15a8810846ac5d6e6103ba0f6e5dc7
Zavarka Team Support <support@zteam.ru>
2018-11-20 02:16:28 +0300

    Полностью переписан код для оценки приложения в маркетах

--- src/core/components/RateThisApp.js
+++ src/core/components/RateThisApp.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
-import {View, Button, Modal} from "react-native";
+import {View, Button, Modal, Text, TouchableHighlight, Alert} from "react-native";
 import Rate, { AndroidMarket } from 'react-native-rate';
 
 export default class RateThisApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rated: false
-        }
-    }
-
-    componentDidMount() {
-        let options = {
-            AppleAppID: "515931794",
-            GooglePackageName: "com.atlantm",
-            preferredAndroidMarket: AndroidMarket.Google,
-            preferInApp: true,
-            openAppStoreIfInAppFails: false,
-        };
-        Rate.rate(options, success => {
-            if (success) {
-                // this technically only tells us if the user successfully went to the Review Page. Whether they actually did anything, we do not know.
-                this.setState({rated: true});
-            }
-        });
-    }
 
     render() {
+        Alert.alert(
+            'Нравится Атлант-М?',
+            'Расскажите миру об удобстве пользования этим приложением!',
+            [
+                // {text: 'Позже', onPress: () => {this.props.onAskLater && this.props.onAskLater()}, style: 'cancel'},
+                {text: 'Не напоминать', onPress: () => {this.props.onSuccess && this.props.onSuccess()}, style: 'cancel'},
+                {text: 'Оценить', onPress: () => {
+                    let options = {
+                        AppleAppID: "515931794",
+                        GooglePackageName: "com.atlantm",
+                        preferredAndroidMarket: AndroidMarket.Google,
+                        preferInApp: true,
+                        openAppStoreIfInAppFails: false,
+                    };
+
+                    Rate.rate(options, success => {
+                        if (success) {
+                            this.props.onSuccess && this.props.onSuccess();
+                        }
+                    });
+                }},
+            ],
+            { cancelable: false }
+        )
+
         return null;
     }
 }
