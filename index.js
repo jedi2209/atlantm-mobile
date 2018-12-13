3ae41a422e6e894c825f3cc8f717023ff1dcd4b8
Zavarka Team Support <support@zteam.ru>
2018-12-13 03:35:06 +0300

    Рефакторинг кода

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppRegistry, YellowBox } from 'react-native';
 import { Sentry } from 'react-native-sentry';
+import Wrapper from './src/core/containers/Wrapper';
 
 YellowBox.ignoreWarnings([
   'Remote debugger',
@@ -8,8 +9,6 @@ YellowBox.ignoreWarnings([
   'Warning: componentWillReceiveProps is deprecated',
   'Module RNDeviceInfo requires main queue setup since it overrides',
 ]);
-
-import Wrapper from './src/core/containers/Wrapper';
 
 Sentry
   .config('https://2e35f2a2455b4a3d97a1687270845d33:4df609d533fd4ce3be4fa721e6583c87@sentry.io/219899')
