e37e365ee3cd0a5d4e9f206602aa457bddd9aad5
Zavarka Team Support <support@zteam.ru>
2019-04-06 00:11:58 +0300

    Version update for iOS fix

--- version.sh
+++ version.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash -e
 
 PROJECT_DIR="ios/atlantm"
+ONESIGNAL_DIR="ios/OneSignalNotificationServiceExtension"
 INFOPLIST_FILE="Info.plist"
 INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
+INFOPLIST_ONESIGNAL_DIR="${ONESIGNAL_DIR}/${INFOPLIST_FILE}"
 ANDROID_VERSION_FILE="android/app/versioning/app_version.properties"
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
@@ -14,6 +16,10 @@ BUILD_NUMBER=0
 ## Update plist with new values
 /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_DIR}"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_DIR}"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_ONESIGNAL_DIR}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_ONESIGNAL_DIR}"
+
 
 ## Set Android version to 0
 echo "versionCode=0" > $ANDROID_VERSION_FILE
