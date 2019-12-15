1cf4be6a1292e231cfbe411cc56fe6937a496b5d
anyatyu <anyatyu@yandex-team.ru>
2019-12-16 01:12:13 +0300

    починили падение на андроиде

--- src/utils/amplitude-analytics.js
+++ src/utils/amplitude-analytics.js
@@ -1,26 +1,10 @@
-import amplitude from 'amplitude-js';
-// import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
+const amplitude = require('amplitude-js');
 
 export default class Amplitude {
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new amplitude('2716d7eebc63593e80e4fd172fc8b6f3');
-    }
-    return this.instance;
-  }
-
-  static getGATracker() {
-    if (!this.gatracker) {
-      this.gatracker = new GoogleAnalyticsTracker('UA-118347995-1');
-    }
-
-    return this.gatracker;
-  }
-
   static logEvent(category, action, params) {
     if (!__DEV__) {
-      this.getInstance().logEvent(`${category}:${action}`, params);
-      // this.getGATracker().trackEvent(category, action, params);
+      amplitude.getInstance().setUserId('2716d7eebc63593e80e4fd172fc8b6f3');
+      amplitude.getInstance().logEvent(`${category}:${action}`, params);
     }
   }
 }
