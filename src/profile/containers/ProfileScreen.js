6e2b09a1fc4bf6aefffe358c4c465cd441b933ae
anyatyu <anyatyu@yandex-team.ru>
2019-12-21 17:45:15 +0300

    google oath

--- src/profile/containers/ProfileScreen.js
+++ src/profile/containers/ProfileScreen.js
@@ -163,9 +163,31 @@ const mapDispatchToProps = {
   actionSetPushActionSubscribe,
 };
 
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/userinfo.email'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId: '53201529704-4fl35lhveh4lvcdj9o3nli0fpk8c942r.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+  forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+  accountName: '', // [Android] specifies an account name on the device that should be used
+  iosClientId:
+    '53201529704-pofi5bbpvo7dtnuu521lo00f3bl6qiq2.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+});
+
 class ProfileScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isSigninInProgress: false,
+    };
 
     console.log(GraphRequestManager);
     this.requestManager = new GraphRequestManager();
@@ -317,6 +339,26 @@ class ProfileScreen extends Component {
     });
   }
 
+  // Somewhere in your code
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('userInfo', userInfo);
+      this.setState({userInfo});
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   render() {
     return (
       <KeyboardAvoidingView behavior="position">
@@ -358,6 +400,13 @@ class ProfileScreen extends Component {
                   }
                 }}
                 onLogoutFinished={() => alert('User logged out')}
+              />
+              <GoogleSigninButton
+                style={{width: 192, height: 48}}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={this._signIn}
+                disabled={this.state.isSigninInProgress}
               />
               <Button
                 iconLeft
