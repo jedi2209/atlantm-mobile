24a80a7a4e143d64c8b3f3732582101cb88d1aeb
Aleksandr Kvasha <het2000@mail.ru>
2020-01-05 01:23:24 +0300

    Added android script to 'npm run android' to choose Android Emulator

---
+++ android/dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printf "This is Your Android Devices that you can choose to start \n";
+emulators=`$(echo emulator -list-avds)`;
+while read -r line; do lines+=("$line"); done <<< "$emulators"
+
+printf "Which one of them you want to start?\r\n\r\n";
+
+select opt in "${lines[@]}";
+do
+    case $opt in
+        *) break;;
+    esac
+done
+
+printf "OK, started Android emulator based on $opt \r\n";
+exec $HOME/Library/Android/sdk/emulator/emulator -avd $opt -no-snapshot -wipe-data -logcat *:e -verbose
