bb7a5b9f182938272a5e1394cecc3733e42a2a30
anyatyu <anyatyu@yandex-team.ru>
2020-01-08 01:25:36 +0300

    keyboard bug

--- src/core/components/KeyboardAvoidingView.js
+++ src/core/components/KeyboardAvoidingView.js
@@ -3,13 +3,16 @@ import {
   KeyboardAvoidingView as KeyboardAvoidingNativeView,
   Platform,
 } from 'react-native';
+import {Header} from 'react-navigation-stack';
+
+const offset = Header.HEIGHT + 28;
 
 export const KeyboardAvoidingView = ({children}) => (
   <KeyboardAvoidingNativeView
     style={{flex: 1}}
     contentContainerStyle={{flex: 1}}
     behavior={Platform.select({ios: 'position', android: null})}
-    keyboardVerticalOffset={0}>
+    keyboardVerticalOffset={-offset}>
     {children}
   </KeyboardAvoidingNativeView>
 );
