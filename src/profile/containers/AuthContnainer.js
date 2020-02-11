b52bbe957199f09fd4d5f8ae08c7a6ed8f20a4a3
anyatyu <anyatyu@yandex-team.ru>
2020-02-11 16:15:45 +0300

    fix white screen at profile

---
+++ src/profile/containers/AuthContnainer.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {withNavigation} from 'react-navigation';
+
+import Login from './ProfileScreen';
+import Profile from './ProfileScreenInfo';
+import {connect} from 'react-redux';
+
+const LoginScreen = withNavigation(Login);
+const ProfileScreen = withNavigation(Profile);
+
+const mapStateToProps = ({profile}) => {
+  return profile;
+};
+
+class AuthContnainer extends Component {
+  static navigationOptions = ({navigation}) => ({
+    header: null,
+  });
+
+  render() {
+    if (this.props.login.id) {
+      return <ProfileScreen />;
+    }
+    return <LoginScreen />;
+  }
+}
+
+export default connect(mapStateToProps)(AuthContnainer);
