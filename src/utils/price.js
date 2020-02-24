b81cc5aef049d21f63a1936782cb3ba3050ceae4
Aleksandr Kvasha <het2000@mail.ru>
2020-02-24 16:24:20 +0300

    Fix for price currency

--- src/utils/price.js
+++ src/utils/price.js
@@ -4,7 +4,8 @@ export default function(price, country) {
   }
 
   let country_code = 'be-BE',
-    currency_code = 'BYN';
+    currency_code = 'BYN',
+    currency_show = 'symbol';
 
   switch (country.toLowerCase()) {
     case 'ru':
@@ -12,6 +13,7 @@ export default function(price, country) {
     case 'rur':
       country_code = 'ru-RU';
       currency_code = 'RUB';
+      currency_show = 'symbol';
       // currency_code = 'руб.';
       break;
     case 'by':
@@ -19,18 +21,20 @@ export default function(price, country) {
       country_code = 'be-BE';
       // currency_code = 'byn';
       currency_code = 'BYN';
+      currency_show = 'code';
       break;
     case 'ua':
     case 'uah':
       country_code = 'ua-UA';
       // currency_code = 'грн.';
       currency_code = 'UAH';
+      currency_show = 'code';
       break;
   }
 
   return parseInt(price, 10).toLocaleString(country_code, {
     style: 'currency',
-    currencyDisplay: 'symbol',
+    currencyDisplay: currency_show,
     currency: currency_code,
     minimumFractionDigits: 0,
   });
