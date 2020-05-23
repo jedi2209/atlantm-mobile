534826332ceb2515e2a525fe4d77edf866ae1ac5
Aleksandr Kvasha <het2000@mail.ru>
2020-05-23 19:53:58 +0300

    Modify Amplitude init

--- src/utils/amplitude-analytics.js
+++ src/utils/amplitude-analytics.js
@@ -1,9 +1,17 @@
+import {store} from '@core/store';
+import {get} from 'lodash';
+
 const amplitude = require('amplitude-js');
 
 export default class Amplitude {
   static logEvent(category, action, params) {
+    const SAPID = get(store.getState(), 'profile.login.SAP.ID');
+    const UserID = get(store.getState(), 'profile.login.id');
     if (!__DEV__) {
-      amplitude.getInstance().setUserId('XXXX');
+      amplitude.getInstance().init('XXXX');
+      if (SAPID || UserID) {
+        amplitude.getInstance().setUserId(SAPID ? SAPID : UserID);
+      }
       amplitude.getInstance().logEvent(`${category}:${action}`, params);
     }
   }
