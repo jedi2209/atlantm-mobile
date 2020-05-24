e58128e67b4f530e1a98b8922ce48670ba8ffe5e
Aleksandr Kvasha <het2000@mail.ru>
2020-05-25 00:44:05 +0300

    Rename OLD unused component

--- src/service/components/_old_ServiceForm.js
+++ src/service/components/_old_ServiceForm.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
-import { View, StyleSheet, Platform } from 'react-native';
+import React, {PureComponent} from 'react';
+import {View, StyleSheet, Platform} from 'react-native';
 import PropTypes from 'prop-types';
 
 // components
-import { ListItem, Body, Item, Label, Input } from 'native-base';
+import {ListItem, Body, Item, Label, Input} from 'native-base';
 import DatePicker from 'react-native-datepicker';
 
 // styles
@@ -17,7 +17,6 @@ const styles = StyleSheet.create({
     alignSelf: Platform.OS === 'ios' ? 'stretch' : 'flex-start',
     flex: 2.1,
   },
-
 });
 const datePickerStyles = {
   dateTouchBody: {
@@ -61,28 +60,33 @@ export default class ServiceForm extends PureComponent {
     date: PropTypes.object,
     carFill: PropTypes.func,
     dateFill: PropTypes.func,
-  }
+  };
 
   static defaultProps = {
     car: '',
     date: {},
-  }
+  };
 
-  onChangeCar = (value) => this.props.carFill(value)
-  onChangeDate = (formatted, date) => this.props.dateFill({
-    date,
-    formatted,
-  })
+  onChangeCar = value => this.props.carFill(value);
+  onChangeDate = (formatted, date) =>
+    this.props.dateFill({
+      date,
+      formatted,
+    });
 
   render() {
-    const { car, date } = this.props;
+    const {car, date} = this.props;
 
     const minDate = new Date();
 
     return (
       <View>
-        <View style={[stylesList.listItemContainer, stylesList.listItemContainerFirst]}>
-          <ListItem style={[stylesList.listItem, stylesList.listItemReset]} >
+        <View
+          style={[
+            stylesList.listItemContainer,
+            stylesList.listItemContainerFirst,
+          ]}>
+          <ListItem style={[stylesList.listItem, stylesList.listItemReset]}>
             <Body>
               <Item style={stylesList.inputItem} fixedLabel>
                 <Label style={stylesList.label}>Авто</Label>
@@ -103,31 +107,29 @@ export default class ServiceForm extends PureComponent {
         </View>
 
         <View style={stylesList.listItemContainer}>
-          <ListItem last style={[stylesList.listItem, stylesList.listItemReset]}>
+          <ListItem
+            last
+            style={[stylesList.listItem, stylesList.listItemReset]}>
             <Body>
               <Item style={stylesList.inputItem} fixedLabel last>
-                  <Label style={[
-                    stylesList.label,
-                    { flex: 1 },
-                  ]}>Дата</Label>
-                  <DatePicker
-                    style={styles.datePicker}
-                    showIcon={false}
-                    date={date.formatted}
-                    mode="date"
-                    minDate={minDate}
-                    placeholder="Выбрать"
-                    format="DD MMMM YYYY"
-                    confirmBtnText="Выбрать"
-                    cancelBtnText="Отмена"
-                    customStyles={datePickerStyles}
-                    onDateChange={this.onChangeDate}
-                  />
-                </Item>
+                <Label style={[stylesList.label, {flex: 1}]}>Дата</Label>
+                <DatePicker
+                  style={styles.datePicker}
+                  showIcon={false}
+                  date={date.formatted}
+                  mode="date"
+                  minDate={minDate}
+                  placeholder="Выбрать"
+                  format="DD MMMM YYYY"
+                  confirmBtnText="Выбрать"
+                  cancelBtnText="Отмена"
+                  customStyles={datePickerStyles}
+                  onDateChange={this.onChangeDate}
+                />
+              </Item>
             </Body>
           </ListItem>
         </View>
-
       </View>
     );
   }
