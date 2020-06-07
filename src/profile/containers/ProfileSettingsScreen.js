34266f35c83fd307aa60c98b868d117ef0815c48
Aleksandr Kvasha <het2000@mail.ru>
2020-06-07 22:40:31 +0300

    Modify user profile edit screen
    Modify Form component
    
    Added DatePickerCustom component

--- src/profile/containers/ProfileSettingsScreen.js
+++ src/profile/containers/ProfileSettingsScreen.js
@@ -12,9 +12,11 @@ import {
 } from 'react-native';
 import {Button} from 'native-base';
 import {connect} from 'react-redux';
+import {dayMonthYear} from '../../utils/date';
 
 import {KeyboardAvoidingView} from '../../core/components/KeyboardAvoidingView';
-import {TextInput} from '../../core/components/TextInput';
+import Form from '../../core/components/Form/Form';
+import SocialAuth from '../components/SocialAuth';
 
 import {actionSaveProfileByUser} from '../actions';
 
@@ -27,6 +29,7 @@ class ProfileSettingsScreen extends Component {
   static navigationOptions = ({navigation}) => ({
     headerStyle: stylesHeader.whiteHeader,
     headerTitleStyle: stylesHeader.whiteHeaderTitle,
+    headerTitle: 'Редактирование профиля',
     headerLeft: <HeaderIconBack theme="blue" navigation={navigation} />,
     headerRight: <View />,
   });
@@ -43,7 +46,7 @@ class ProfileSettingsScreen extends Component {
         model: '',
       };
     } else {
-      car = this.props.profile.cars.find(value => value.owner) || {
+      car = this.props.profile.cars.find((value) => value.owner) || {
         number: '',
         brand: '',
         model: '',
@@ -53,30 +56,151 @@ class ProfileSettingsScreen extends Component {
     console.log('this.props.profile', this.props.profile);
 
     const {
-      first_name,
-      last_name,
-      second_name,
-      email,
-      phone,
+      NAME,
+      LAST_NAME,
+      SECOND_NAME,
+      EMAIL,
+      PHONE,
+      BIRTHDATE,
     } = this.props.profile;
 
+    let emailData = [];
+    let phoneData = [];
+    let birthdate = '';
+
+    if (typeof EMAIL === 'object' && EMAIL.length) {
+      EMAIL.map((field, num) => {
+        emailData[field.ID] = {
+          id: field.ID,
+          name: 'EMAIL',
+          type: 'email',
+          label: 'E-mail',
+          value: field.VALUE,
+        };
+      });
+    } else {
+      emailData = EMAIL;
+    }
+
+    if (typeof PHONE === 'object' && PHONE.length) {
+      PHONE.map((field, num) => {
+        phoneData[field.ID] = {
+          id: field.ID,
+          name: 'PHONE',
+          type: 'phone',
+          label: 'Телефон',
+          value: field.VALUE,
+          country: field.COUNTRY,
+        };
+      });
+    } else {
+      phoneData = PHONE;
+    }
+
+    if (BIRTHDATE) {
+      birthdate = dayMonthYear(new Date(BIRTHDATE));
+    }
+
     this.state = {
-      lastName: last_name || '',
-      firstName: first_name || '',
-      secondName: second_name || '',
-      email: email ? email.value : '',
-      phone: phone ? phone.value : '',
+      firstName: NAME || '',
+      secondName: SECOND_NAME || '',
+      lastName: LAST_NAME || '',
+      birthdate: birthdate || '',
+      email: emailData || [],
+      phone: phoneData || [],
       car: car.brand && car.model ? `${car.brand} ${car.model}` : '',
       carNumber: car.number,
       success: false,
     };
+
+    console.log('this.state', this.state);
+
+    this.FormConfig = {
+      fields: {
+        groups: [
+          {
+            name: 'Основное',
+            fields: [
+              {
+                name: 'NAME',
+                type: 'input',
+                label: 'Имя',
+                value: this.state.firstName,
+                props: {
+                  textContentType: 'name',
+                },
+              },
+              {
+                name: 'SECOND_NAME',
+                type: 'input',
+                label: 'Отчество',
+                value: this.state.secondName,
+                props: {
+                  textContentType: 'name',
+                },
+              },
+              {
+                name: 'LAST_NAME',
+                type: 'input',
+                label: 'Фамилия',
+                value: this.state.lastName,
+                props: {
+                  textContentType: 'name',
+                },
+              },
+            ],
+          },
+          {
+            name: 'Контактные данные',
+            fields: [].concat(this.state.email, this.state.phone),
+          },
+          {
+            name: 'Ваши соц.сети для быстрого входа',
+            fields: [
+              {
+                name: 'SocialAuth',
+                type: 'component',
+                label: 'Привяжите соц.сети',
+                value: (
+                  <SocialAuth
+                    region={this.props.dealerSelected.region}
+                    style={{
+                      width: '80%',
+                      marginHorizontal: '10%',
+                      paddingVertical: 7,
+                    }}
+                  />
+                ),
+              },
+            ],
+          },
+          {
+            name: 'Дополнительно',
+            fields: [
+              {
+                name: 'BIRTHDATE',
+                type: 'date',
+                label: 'Когда у вас день рождения?',
+                value: this.state.birthdate,
+                props: {
+                  placeholder: null,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    };
+    console.log('this.FormConfig', this.FormConfig);
   }
 
   componentDidMount() {
     Amplitude.logEvent('screen', 'profile/edit');
   }
 
-  onPressSave = () => {
+  onPressSave = (props) => {
+    console.log('this.props222', props);
+    return false;
     this.setState({loading: true});
     const {name, email, phone, id} = this.props.profile;
     let emailValue;
@@ -128,7 +252,7 @@ class ProfileSettingsScreen extends Component {
         carNumber: this.state.carNumber,
         car: this.state.car,
       })
-      .then(data => {
+      .then((data) => {
         this.setState({success: true, loading: false});
         const _this = this;
         Alert.alert('Ваши данные успешно сохранены', '', [
@@ -149,92 +273,21 @@ class ProfileSettingsScreen extends Component {
       });
   };
 
-  onChangeProfileField = fieldName => value => {
+  onChangeProfileField = (fieldName) => (value) => {
     this.setState({[fieldName]: value});
   };
 
   render() {
     return (
       <KeyboardAvoidingView>
-        <StatusBar barStyle="dark-content" />
-        <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
+        <ScrollView style={{flex: 1, backgroundColor: '#eee'}}>
           <View style={styles.container}>
-            <View style={styles.header}>
-              <Text style={styles.heading}>Изменение данных</Text>
-            </View>
-            <View style={styles.group}>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Фамилия"
-                  value={this.state.lastName}
-                  onChangeText={this.onChangeProfileField('lastName')}
-                />
-              </View>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Имя"
-                  value={this.state.firstName}
-                  onChangeText={this.onChangeProfileField('firstName')}
-                />
-              </View>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Отчество"
-                  value={this.state.secondName}
-                  onChangeText={this.onChangeProfileField('secondName')}
-                />
-              </View>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Email"
-                  value={this.state.email}
-                  onChangeText={this.onChangeProfileField('email')}
-                />
-              </View>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Телефон"
-                  value={this.state.phone}
-                  onChangeText={this.onChangeProfileField('phone')}
-                />
-              </View>
-            </View>
-            <View style={styles.group}>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Авто"
-                  value={this.state.car}
-                  onChangeText={this.onChangeProfileField('car')}
-                />
-              </View>
-              <View style={styles.field}>
-                <TextInput
-                  style={styles.textinput}
-                  label="Гос. номер"
-                  value={this.state.carNumber}
-                  onChangeText={this.onChangeProfileField('carNumber')}
-                />
-              </View>
-            </View>
-            <View style={styles.group}>
-              <Button
-                onPress={
-                  this.state.loading ? undefined : () => this.onPressSave()
-                }
-                style={[styleConst.shadow.default, styles.button]}>
-                {this.state.loading ? (
-                  <ActivityIndicator color="#fff" />
-                ) : (
-                  <Text style={styles.buttonText}>Сохранить</Text>
-                )}
-              </Button>
-            </View>
+            <Form
+              fields={this.FormConfig.fields}
+              barStyle={'light-content'}
+              defaultCountryCode={'by'}
+              onSubmit={this.onPressSave}
+            />
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
@@ -242,9 +295,10 @@ class ProfileSettingsScreen extends Component {
   }
 }
 
-const mapStateToProps = ({profile}) => {
+const mapStateToProps = ({profile, dealer}) => {
   return {
     profile: profile.login,
+    dealerSelected: dealer.selected,
   };
 };
 
@@ -262,36 +316,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 20,
     paddingHorizontal: 14,
-    backgroundColor: '#fff',
-  },
-  header: {
-    marginBottom: 36,
-  },
-  heading: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  field: {
-    marginBottom: 18,
-  },
-  group: {
-    marginBottom: 36,
-  },
-  textinput: {
-    height: Platform.OS === 'ios' ? 40 : 'auto',
-    borderColor: '#d8d8d8',
-    borderBottomWidth: 1,
-    color: '#222b45',
-    fontSize: 18,
-  },
-  button: {
-    backgroundColor: styleConst.color.lightBlue,
-    borderRadius: 5,
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    textTransform: 'uppercase',
-    fontSize: 16,
+    // backgroundColor: '#fff',
   },
 });
