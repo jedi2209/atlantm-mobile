af5df838890366056417c070c73af2ff8fd73e4a
Aleksandr Kvasha <het2000@mail.ru>
2020-06-26 02:36:57 +0300

    Add Badge component

---
+++ src/core/components/Badge.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import styleConst from '../style-const';
+
+const styles = StyleSheet.create({
+  badgeContainer: {
+    paddingHorizontal: 5,
+    paddingVertical: 3,
+    borderRadius: 5,
+    marginRight: 3,
+  },
+  badgeName: {
+    fontFamily: styleConst.font.regular,
+    fontSize: 12,
+  },
+});
+
+const Badge = (props) => {
+  return (
+    <View style={[styles.badgeContainer, {backgroundColor: props.bgColor}]}>
+      <Text style={[styles.badgeName, {color: props.textColor}]}>
+        {props.name}
+      </Text>
+    </View>
+  );
+};
+
+Badge.propTypes = {
+  name: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+};
+
+Badge.defaultProps = {
+  textColor: 'black',
+};
+
+export default Badge;
