9a343e0b686936394883cbbdfc5d7175b34ec837
Aleksandr Kvasha <het2000@mail.ru>
2020-08-03 13:09:08 +0300

    NPM modules update
    Service user fix

--- src/utils/user.js
+++ src/utils/user.js
@@ -5,6 +5,12 @@ export default class UserData {
   static get(param) {
     let localData = get(store.getState(), 'profile.localUserData.' + param);
     let liveData = get(store.getState(), 'profile.login.' + param);
+    if (
+      (typeof liveData === 'undefined' || liveData === null) &&
+      (typeof localData === 'undefined' || localData === null)
+    ) {
+      return false;
+    }
     if (typeof localData === 'object') {
       localData = localData[0];
     }
