543f5ddac4b1cabe382213de0077ef873e4bcd53
Aleksandr Kvasha <het2000@mail.ru>
2020-08-19 23:53:50 +0300

    Update redux npm module

--- src/core/store.js
+++ src/core/store.js
@@ -1,24 +1,18 @@
 import thunkMiddleware from 'redux-thunk';
-import thunk from 'redux-thunk';
 import {autoRehydrate} from 'redux-persist';
 import {createStore, compose, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger';
 
 import rootReducer from './reducers';
 
-const middleware = [
-  thunkMiddleware,
-  __DEV__ && createLogger({collapsed: true}),
-].filter(Boolean);
-
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// export const store = createStore(
-//   rootReducer,
-//   compose(applyMiddleware(...middleware), ),
-// );
+const middleware = [
+  thunkMiddleware,
+  __DEV__ && createLogger({collapsed: true, diff: true}),
+];
 
 export const store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(thunk), autoRehydrate()),
+  composeEnhancer(applyMiddleware(...middleware), autoRehydrate()),
 );
