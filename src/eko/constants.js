c69f05d7aa62d9e3c9d143da1298a98132ce62ef
Aleksandr Kvasha <het2000@mail.ru>
2020-10-07 23:26:36 +0300

    Modify EKO new feedback form

--- src/eko/constants.js
+++ src/eko/constants.js
@@ -3,11 +3,11 @@ export const TEXT_RATE_APP = 'Если тебе понравилось наше 
 export const TEXT_REVIEW_ADD_QUESTION__PLUS = 'Что тебе понравилось в работе автоцентра? Кому бы ты хотел выразить благодарность?';
 export const TEXT_REVIEW_ADD_QUESTION__MINUS = 'Чем ты остался недоволен в работе автоцентра?';
 
-export const REVIEW_ADD_RATING_5 = 'В высшей степени доволен';
-export const REVIEW_ADD_RATING_4 = 'Очень доволен';
-export const REVIEW_ADD_RATING_3 = 'Вполне доволен';
-export const REVIEW_ADD_RATING_2 = 'Не очень доволен';
-export const REVIEW_ADD_RATING_1 = 'Совсем недоволен';
+export const REVIEW_ADD_RATING_5 = '⭐⭐⭐⭐⭐';
+export const REVIEW_ADD_RATING_4 = '⭐⭐⭐⭐';
+export const REVIEW_ADD_RATING_3 = '⭐⭐⭐';
+export const REVIEW_ADD_RATING_2 = '⭐⭐';
+export const REVIEW_ADD_RATING_1 = '⭐';
 
 export const REVIEWS_FILTER_DATE_PERIOD__ALL = 'все';
 export const REVIEWS_FILTER_DATE_PERIOD__WEEK = 'неделя';
