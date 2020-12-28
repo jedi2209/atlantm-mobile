e07783022184a324a6e8ccb41674c57644b916ef
Zavarka Team Support <support@zavarka.team>
2020-12-28 21:34:36 +0300

    Remove sentry from DEV environment

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ import {AppRegistry, LogBox} from 'react-native';
 import * as Sentry from '@sentry/react-native';
 import Wrapper from './src/core/containers/Wrapper';
 
-LogBox.ignoreLogs([
-  'left was given',
-  'right was given',
-  'Remote debugger',
-  'Warning: componentWillUpdate has been renamed',
-  'Warning: componentWillReceiveProps',
-  'Module RNDeviceInfo requires main queue setup since it overrides',
-]);
+// LogBox.ignoreLogs([
+//   'left was given',
+//   'right was given',
+//   'Remote debugger',
+//   'Warning: componentWillUpdate has been renamed',
+//   'Warning: componentWillReceiveProps',
+//   'Module RNDeviceInfo requires main queue setup since it overrides',
+// ]);
 
-Sentry.init({
-  dsn: 'https://XXXX@sentry.io/219899',
-});
+if (!__DEV__) {
+  Sentry.init({
+    dsn: 'https://XXXX@sentry.io/219899',
+  });
+}
 
 // const AtlantmApplication = () => <Wrapper />;
 
