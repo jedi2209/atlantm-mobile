31a676e2dad115ced485f0583d02dc2f41293f29
Zavarka Team Support <support@zavarka.team>
2021-05-21 09:59:22 +0300

    codesigndoc_exports add to .gitignore
    Minor fix for amplitude

--- src/utils/amplitude-analytics.js
+++ src/utils/amplitude-analytics.js
@@ -4,14 +4,15 @@ import {Amplitude} from '@amplitude/react-native';
 
 export default class Analytics {
   static logEvent(category, action, params) {
+    if (__DEV__) {
+      return true;
+    }
     const SAPID = get(store.getState(), 'profile.login.SAP.ID');
     const UserID = get(store.getState(), 'profile.login.id');
-    if (!__DEV__) {
-      Amplitude.getInstance().init('2716d7eebc63593e80e4fd172fc8b6f3');
-      if (SAPID || UserID) {
-        Amplitude.getInstance().setUserId(SAPID ? SAPID : UserID);
-      }
-      Amplitude.getInstance().logEvent(`${category}:${action}`, params);
+    Amplitude.getInstance().init('2716d7eebc63593e80e4fd172fc8b6f3');
+    if (SAPID || UserID) {
+      Amplitude.getInstance().setUserId(SAPID ? SAPID : UserID);
     }
+    Amplitude.getInstance().logEvent(`${category}:${action}`, params);
   }
 }
