c0612d473a6e74875b712c72803bbe3f757db469
Zavarka Team Support <support@zavarka.team>
2021-06-09 19:24:09 +0300

    Final init detox for iOS

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     window: {},
   },
   verbose: true,
-  testRegex: '(/src/__tests__/.*|\\.(-test|spec))\\.(ts|tsx|js)$',
+  testRegex: '(/src/__tests__/.*|\\.(-test|spec|e2e))\\.(ts|tsx|js)$',
   setupFiles: [
     '<rootDir>/jest.setup.js',
     '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
@@ -17,6 +17,17 @@ module.exports = {
   testEnvironment: 'node',
   transformIgnorePatterns: [
     resolve(__dirname, '../../packages'),
-    'node_modules/(?!(jest-)?react-native|react-navigation|native-base|native-base-shoutem-theme|@shoutem/theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)',
+    "node_modules/(?!(react-native"
+    + "|native-base"
+    + "|native-base-shoutem-theme"
+    + "|@shoutem/theme"
+    + "|@shoutem/animation"
+    + "|@shoutem/ui"
+    + "|tcomb-form-native"
+    + "|react-navigation-tabs"
+    + "|react-native-splash-screen"
+    + "|react-native-screens"
+    + "|react-native-reanimated"
+    + ")/)",
   ],
 };
