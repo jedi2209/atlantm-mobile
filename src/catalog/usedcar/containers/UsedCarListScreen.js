8643859badb78ba3970e7f7a7f710030e47536e7
Zavarka Team Support <support@zavarka.team>
2021-08-16 15:05:20 +0300

    Minor core reformat

--- src/catalog/usedcar/containers/UsedCarListScreen.js
+++ src/catalog/usedcar/containers/UsedCarListScreen.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect, useReducer} from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, StatusBar, ActivityIndicator} from 'react-native';
 
 // redux
