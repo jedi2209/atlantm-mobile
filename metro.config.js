46d506ff818de20614c0b7549e7da345b2488c09
Zavarka Team <support@zavarka.team>
2022-08-08 18:16:19 +0300

    Update Android to a new RN version

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const OriginalResolver = require("metro-resolver");
-const path = require("path");
+// const OriginalResolver = require('metro-resolver');
+// const path = require('path');
 
-const blacklistedModules = ["https", "http", "zlib", "ansi-styles"];
+// const blacklistedModules = ['https', 'http', 'zlib', 'ansi-styles'];
 
 module.exports = {
   transformer: {
@@ -11,31 +11,6 @@ module.exports = {
         inlineRequires: true,
       },
     }),
-    minifierConfig: {
-      keep_classnames: true, // Preserve class names
-      keep_fnames: true, // Preserve function names
-      mangle: {
-        keep_classnames: true, // Preserve class names
-        keep_fnames: true, // Preserve function names
-      },
-    },
     assetPlugins: ['react-native-svg-asset-plugin'],
-  },
-  resolver: {
-    useWatchman: true,
-    resolveRequest: (context, realModuleName, platform, moduleName) => {
-      if (blacklistedModules.includes(moduleName)) {
-        return {
-          filePath: path.resolve(__dirname + "/src/shim-module.js"),
-          type: "sourceFile"
-        };
-      } else {
-        return OriginalResolver.resolve(
-          { ...context, resolveRequest: undefined },
-          moduleName,
-          platform
-        );
-      }
-    }
   },
 };
