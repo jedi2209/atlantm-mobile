96c51fe3108253435efd83ba895e9066ea7a5c4e
Zavarka Team <support@zavarka.team>
2023-07-11 01:03:12 -0700

    Re-init e2e detox tests

--- src/__tests__/init/Init APP.e2e.js
+++ src/__tests__/init/Init APP.e2e.js
@@ -1,13 +1,13 @@
 import {initApp} from './init.app.spec';
 
 describe('APP init', () => {
-	beforeAll(async () => {
-		await device.launchApp({
-			permissions: {
-				notifications: 'YES'
-			}
-		});
-	});
+  beforeAll(async () => {
+    await device.launchApp({
+      permissions: {
+        notifications: 'YES',
+      },
+    });
+  });
 
-	initApp(112, true);
+  initApp(112, true);
 });
--- src/__tests__/init/init.app.spec.js
+++ src/__tests__/init/init.app.spec.js
@@ -1,38 +1,42 @@
 export const initApp = (dealer = 129, fisrtInit = false) => {
-    return describe('Init APP', () => {
-        it('Choose Dealer Button Exist', async () => {
-            await expect(element(by.id("IntroChooseDealerButton"))).toBeVisible();
-            await element(by.id("IntroChooseDealerButton")).tap();
-        });
-
-        it('All Countries should be loaded', async () => {
-            await expect(element(by.text('🇧🇾 Беларусь'))).toExist();
-            await expect(element(by.text('🇷🇺 Россия'))).toExist();
-            await expect(element(by.text('🇺🇦 Україна'))).toExist();
-        });
-
-        if (fisrtInit) {
-            it('Tabs must be successfully clicked', async() => {
-                await element(by.text('🇷🇺 Россия')).tap();
-                await expect(element(by.text("Атлант-М Бажова"))).toBeVisible();
-                await element(by.text('🇺🇦 Україна')).tap();
-                await expect(element(by.text("Атлант-М Київ"))).toBeVisible();
-                await element(by.text('🇧🇾 Беларусь')).tap();
-                await expect(element(by.text("Атлант-М на Независимости"))).toBeVisible();
-            });
-        }
-
-        it('Dealer successfull choose', async() => {
-            await element(by.id("DealerCard_" + dealer)).tap();
-            await expect(element(by.id('ContactsScreen.currentActionsHeading'))).toBeVisible();
-        });
+  return describe('Init APP', () => {
+    it('Choose Dealer Button Exist', async () => {
+      await expect(element(by.id('IntroChooseDealerButton'))).toBeVisible();
+      await element(by.id('IntroChooseDealerButton')).tap();
     });
-}
 
-export const launchApp = (params) => {
-    return device.launchApp({
-        permissions: {
-            notifications: 'YES'
-        }
+    it('All Countries should be loaded', async () => {
+      await expect(element(by.text('🇧🇾 Беларусь'))).toExist();
+      await expect(element(by.text('🇷🇺 Россия'))).toExist();
+      await expect(element(by.text('🇺🇦 Україна'))).toExist();
     });
-}
+
+    if (fisrtInit) {
+      it('Tabs must be successfully clicked', async () => {
+        await element(by.text('🇷🇺 Россия')).tap();
+        await expect(element(by.text('Атлант-М Бажова'))).toBeVisible();
+        await element(by.text('🇺🇦 Україна')).tap();
+        await expect(element(by.text('Атлант-М Київ'))).toBeVisible();
+        await element(by.text('🇧🇾 Беларусь')).tap();
+        await expect(
+          element(by.text('Атлант-М на Независимости')),
+        ).toBeVisible();
+      });
+    }
+
+    it('Dealer successfull choose', async () => {
+      await element(by.id('DealerCard_' + dealer)).tap();
+      await expect(
+        element(by.id('ContactsScreen.currentActionsHeading')),
+      ).toBeVisible();
+    });
+  });
+};
+
+export const launchApp = params => {
+  return device.launchApp({
+    permissions: {
+      notifications: 'YES',
+    },
+  });
+};
