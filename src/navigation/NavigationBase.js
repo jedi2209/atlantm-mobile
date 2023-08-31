2403559efe4f10c987bce15a3faef9b1b1a3e649
Zavarka Team <support@zavarka.team>
2023-08-31 21:29:00 +0200

    Fix close button on DealerInfoScreen

--- src/navigation/NavigationBase.js
+++ src/navigation/NavigationBase.js
@@ -179,6 +179,11 @@ export const Base = ({navigation, route}) => {
             {
               icon: 'close',
               iconSize: 12,
+              InnerStyle: {
+                ...stylesHeader.headerBackButtonContainer,
+                width: 50,
+                height: 50,
+              },
             },
           )}
         />
