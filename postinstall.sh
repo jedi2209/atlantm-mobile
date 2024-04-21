4f6bad71ef50192761fae9081e8602100503ee44
Zavarka Team <support@zavarka.team>
2024-04-22 00:08:43 +0200

    Update windows compatibility

--- postinstall.sh
+++ postinstall.sh
@@ -4,8 +4,4 @@ npx patch-package;
 # Detect the current operating system
 if [[ "$OSTYPE" == "darwin"* ]]; then
   yarn pod-update;
-elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
-  # echo "Windows"
-else
-  # echo "Unknown operating system"
 fi
