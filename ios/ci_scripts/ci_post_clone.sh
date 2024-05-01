9c0dd82c4cf822349dec687c8ef7f517cbb29a57
Zavarka Team <support@zavarka.team>
2024-05-02 00:31:18 +0200

    Modify ci_post_clone

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 cd ../../;
+export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+echo 'export PATH="$PATH:$GEM_HOME/bin"' >> ~/.zshrc
+source $HOME/.zshrc
 gem install bundler --user-install
 echo "===== Installling CocoaPods ====="
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
