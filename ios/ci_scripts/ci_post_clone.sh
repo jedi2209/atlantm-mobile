95bcce1ccaff49214906a9487fd6e11c7c7b1f4a
Zavarka Team <support@zavarka.team>
2024-05-02 01:00:10 +0200

    Update rvm config ci_post_clone

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 echo ">>> INSTALL RVM"
-gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --rails
+source $HOME/.rvm/scripts/rvm
 echo ">>> INSTALL RUBY"
 rvm install 3.3.0
-rvm use 3.3.0
+rvm use 3.3.0 --default
 echo ">>> SETUP ENVIRONMENT"
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 echo 'export PATH="$PATH:$GEM_HOME/bin"' >> ~/.zshrc
