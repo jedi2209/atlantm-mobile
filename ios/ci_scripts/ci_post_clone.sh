0253d416408affcb2200484d6ccccf279253f30f
Zavarka Team <support@zavarka.team>
2024-05-02 00:01:17 +0200

    Check pwd

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,19 @@
 #!/bin/zsh
+echo $(pwd)
+# echo "===== Installling CocoaPods ====="
+# export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+# brew install cocoapods
+# echo "===== Installing Node.js ====="
+# brew install node@18
+# echo 'export PATH="/usr/local/opt/node@18/bin:$PATH"' >> ~/.zshrc
+# export LDFLAGS="-L/usr/local/opt/node@18/lib"
+# export CPPFLAGS="-I/usr/local/opt/node@18/include"
+# echo "===== Installing yarn ====="
+# brew install yarn
 
-echo "===== Installling CocoaPods ====="
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install cocoapods
-echo "===== Installing Node.js ====="
-brew install node@18
-echo "===== Installing yarn ====="
-brew install yarn
-
-# Install dependencies
-echo "===== Running yarn install ====="
-yarn install
-echo "===== Running pod install ====="
-cd ios
-pod install
+# # Install dependencies
+# echo "===== Running yarn install ====="
+# yarn install
+# echo "===== Running pod install ====="
+# cd ios
+# pod install
