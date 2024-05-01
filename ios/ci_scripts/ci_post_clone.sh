2b83b7b37d329ebe6901c603d150c11a6d5d2532
Zavarka Team <support@zavarka.team>
2024-05-02 00:56:48 +0200

    Add rvm to ci_post_clone

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
-cd ../../;
+echo ">>> INSTALL RVM"
+gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+echo ">>> INSTALL RUBY"
+rvm install 3.3.0
+rvm use 3.3.0
 echo ">>> SETUP ENVIRONMENT"
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 echo 'export PATH="$PATH:$GEM_HOME/bin"' >> ~/.zshrc
 source $HOME/.zshrc
+cd ../../;
 echo ">>> INSTALL BUNDLER"
 gem install bundler --install-dir $GEM_HOME
 echo ">>> INSTALL DEPENDENCIES"
