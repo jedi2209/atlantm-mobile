bd9d88e0adcb3dd846a46c143769068c6262be7d
Zavarka Team <support@zavarka.team>
2024-06-20 20:48:46 +0200

    Add colors for #MOBILEAPP-3127

--- src/core/components/Badge.js
+++ src/core/components/Badge.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TouchableHighlight, View, Text, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import ModalView from './ModalView';
+
+import {get} from 'lodash';
+
 import styleConst from '../style-const';
 
 const styles = StyleSheet.create({
@@ -15,37 +21,150 @@ const styles = StyleSheet.create({
   },
 });
 
+const BackgroundItem = props => {
+  const {
+    bgColor = styleConst.color.greyText2,
+    badgeContainerStyle = {},
+    children,
+  } = props;
+  if (bgColor.includes('linear-gradient(')) {
+    const regexDeg = new RegExp('([0-9]*)deg', 'gmi');
+    const regexColors = new RegExp('(#[\\w]*) ([\\-0-9.]*)%', 'gmi');
+
+    let m;
+    let linearSettings = {
+      colors: {},
+      degree: 0,
+    };
+
+    while ((m = regexDeg.exec(bgColor)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regexDeg.lastIndex) {
+        regexDeg.lastIndex++;
+      }
+
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        if (groupIndex > 0) {
+          linearSettings['degree'] = parseInt(match);
+        }
+      });
+    }
+
+    while ((m = regexColors.exec(bgColor)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (m.index === regexColors.lastIndex) {
+        regexColors.lastIndex++;
+      }
+
+      // The result can be accessed through the `m`-variable.
+      m.forEach((match, groupIndex) => {
+        if (groupIndex > 0) {
+          if (!linearSettings['colors'][groupIndex]) {
+            linearSettings['colors'][groupIndex] = [];
+          }
+          linearSettings['colors'][groupIndex].push(match);
+        }
+      });
+    }
+
+    return (
+      <LinearGradient
+        start={{
+          x: 1,
+          y: 0,
+        }}
+        end={{
+          x: 0,
+          y: 0,
+        }}
+        useAngle
+        angle={get(linearSettings, 'degree', 0)}
+        colors={get(linearSettings, 'colors.1', [])}
+        style={[
+          styles.badgeContainer,
+          {backgroundColor: bgColor},
+          badgeContainerStyle,
+        ]}>
+        {children}
+      </LinearGradient>
+    );
+  } else {
+    return (
+      <View
+        style={[
+          styles.badgeContainer,
+          {backgroundColor: bgColor},
+          badgeContainerStyle,
+        ]}>
+        {children}
+      </View>
+    );
+  }
+};
+
 const Badge = props => {
-  const {textColor = 'black'} = props;
-  return props?.onPress ? (
-    <TouchableHighlight
-      activeOpacity={0.6}
-      underlayColor="#DDDDDD"
-      onPressOut={props.onPress}
-      style={[
-        styles.badgeContainer,
-        {backgroundColor: props.bgColor},
-        props.badgeContainerStyle,
-      ]}>
-      <Text
-        selectable={false}
-        style={[styles.badgeName, {color: textColor}, props.textStyle]}>
-        {props.name}
-      </Text>
-    </TouchableHighlight>
+  const [isModalVisible, setModalVisible] = useState(false);
+  const {
+    textColor = 'black',
+    bgColor,
+    textStyle = {},
+    descriptionStyle = {},
+    description = null,
+  } = props;
+
+  let onPressCustom;
+
+  if (description) {
+    onPressCustom = () => setModalVisible(true);
+  } else {
+    onPressCustom = props.onPress;
+  }
+
+  return onPressCustom ? (
+    <>
+      <TouchableHighlight
+        activeOpacity={0.6}
+        underlayColor="#DDDDDD"
+        onPressOut={onPressCustom}>
+        <BackgroundItem bgColor={bgColor}>
+          <Text
+            selectable={false}
+            style={[styles.badgeName, {color: textColor}, textStyle]}>
+            {props.name}
+          </Text>
+        </BackgroundItem>
+      </TouchableHighlight>
+      <ModalView
+        isModalVisible={isModalVisible}
+        animationIn="slideInRight"
+        animationOut="slideOutLeft"
+        onHide={() => setModalVisible(false)}
+        selfClosed={true}>
+        <View style={{padding: 10}}>
+          <Text
+            ellipsizeMode="clip"
+            style={[
+              {
+                fontSize: 18,
+                marginBottom: 10,
+                color: styleConst.color.greyText4,
+              },
+              descriptionStyle,
+            ]}>
+            {description}
+          </Text>
+        </View>
+      </ModalView>
+    </>
   ) : (
-    <View
-      style={[
-        styles.badgeContainer,
-        {backgroundColor: props.bgColor},
-        props.badgeContainerStyle,
-      ]}>
+    <BackgroundItem bgColor={bgColor}>
       <Text
         selectable={false}
         style={[styles.badgeName, {color: textColor}, props.textStyle]}>
         {props.name}
       </Text>
-    </View>
+    </BackgroundItem>
   );
 };
 
