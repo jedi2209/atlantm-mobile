539c1374b4825d48790afedd5a3b6c21c58e6db3
Zavarka Team <support@zavarka.team>
2025-01-20 23:35:04 +0100

    Fix bugsnag error

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @format
  */
 import 'react-native-url-polyfill/auto';
-import BugsnagPerformance from '@bugsnag/react-native-performance';
+// import BugsnagPerformance from '@bugsnag/react-native-performance';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import Wrapper from './src/core/containers/Wrapper';
 import {BUGSNAG_ID} from './src/core/const';
 
-BugsnagPerformance.start({apiKey: BUGSNAG_ID});
+// BugsnagPerformance.start({apiKey: BUGSNAG_ID});
 AppRegistry.registerComponent(appName, () => Wrapper);
--- src/utils/trace-errors.js
+++ src/utils/trace-errors.js
@@ -1,10 +1,10 @@
-import Bugsnag from '@bugsnag/react-native';
+// import Bugsnag from '@bugsnag/react-native';
 import * as Sentry from '@sentry/react-native';
 
 export default class TraceInfo {
   static captureException(error) {
     Sentry.captureException(error);
-    Bugsnag.notify(new Error(error));
+    // Bugsnag.notify(new Error(error));
   }
 
   static captureMessage(message) {
