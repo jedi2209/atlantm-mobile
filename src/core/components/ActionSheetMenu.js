f964cb9a998c0dd922b9f4e576ebe8bc234f62bd
Zavarka Team <support@zavarka.team>
2025-01-24 13:39:46 +0100

    Minor Android fixes after RN upgrade #MOBILEAPP-3186

--- src/core/components/ActionSheetMenu.js
+++ src/core/components/ActionSheetMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Linking} from 'react-native';
-import {Text, Icon, Actionsheet, Box, ScrollView} from 'native-base';
+import {Text, Icon, Actionsheet, Box, ScrollView, View} from 'native-base';
 import {useNavigation} from '@react-navigation/native';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -23,74 +23,75 @@ const ActionSheetMenu = props => {
   }
 
   return (
-    <Actionsheet
-      hideDragIndicator={hideDragIndicator}
-      size="full"
-      isOpen={isOpen}
-      onClose={onClose}>
-      <Actionsheet.Content>
-        {actionSheetData?.title ? (
-          <Box w="100%" my={4} px={4} justifyContent="space-between">
-            <Text
-              fontSize="xl"
-              color="gray.800"
-              _dark={{
-                color: 'gray.300',
-              }}>
-              {actionSheetData.title}
-            </Text>
-          </Box>
-        ) : null}
-        <ActionSheetMenuWrapper actionSheetData={actionSheetData}>
-          {actionSheetData.options.map((el, index) => {
-            let fontIcon = Ionicons;
-            switch (el.icon?.font) {
-              case 'FontAwesome5Brands':
-                fontIcon = FontAwesome5Brands;
-                break;
-              default:
-                fontIcon = Ionicons;
-                break;
-            }
-            return (
-              <Actionsheet.Item
-                key={'contactsScreenActionSheet' + index}
-                onPress={() => {
-                  onClose();
-                  if (el.navigate) {
-                    navigation.navigate(el.navigate, el.navigateOptions);
-                  } else {
-                    if (el?.link) {
-                      Linking.openURL(el.link);
+    <View>
+      <Actionsheet
+        hideDragIndicator={hideDragIndicator}
+        isOpen={isOpen}
+        onClose={onClose}>
+        <Actionsheet.Content>
+          {actionSheetData?.title ? (
+            <Box w="100%" my={4} px={4} justifyContent="space-between">
+              <Text
+                fontSize="xl"
+                color="gray.800"
+                _dark={{
+                  color: 'gray.300',
+                }}>
+                {actionSheetData.title}
+              </Text>
+            </Box>
+          ) : null}
+          <ActionSheetMenuWrapper actionSheetData={actionSheetData}>
+            {actionSheetData.options.map((el, index) => {
+              let fontIcon = Ionicons;
+              switch (el.icon?.font) {
+                case 'FontAwesome5Brands':
+                  fontIcon = FontAwesome5Brands;
+                  break;
+                default:
+                  fontIcon = Ionicons;
+                  break;
+              }
+              return (
+                <Actionsheet.Item
+                  key={'contactsScreenActionSheet' + index}
+                  onPress={() => {
+                    onClose();
+                    if (el.navigate) {
+                      navigation.navigate(el.navigate, el.navigateOptions);
+                    } else {
+                      if (el?.link) {
+                        Linking.openURL(el.link);
+                      }
                     }
-                  }
-                }}
-                _text={{
-                  fontSize: 'md',
-                  color: 'gray.600',
-                  w: '100%',
-                }}
-                startIcon={
-                  el.icon ? (
-                    <Icon
-                      as={fontIcon}
-                      color={el.icon?.color}
-                      mr="1"
-                      size={el.icon?.size ? el.icon.size : 6}
-                      name={el.icon?.name}
-                    />
-                  ) : null
-                }>
-                {el?.text}
-                {el?.subtitle ? (
-                  <Text color={'gray.500'}>{el.subtitle}</Text>
-                ) : null}
-              </Actionsheet.Item>
-            );
-          })}
-        </ActionSheetMenuWrapper>
-      </Actionsheet.Content>
-    </Actionsheet>
+                  }}
+                  _text={{
+                    fontSize: 'md',
+                    color: 'gray.600',
+                    w: '100%',
+                  }}
+                  startIcon={
+                    el.icon ? (
+                      <Icon
+                        as={fontIcon}
+                        color={el.icon?.color}
+                        mr="1"
+                        size={el.icon?.size ? el.icon.size : 6}
+                        name={el.icon?.name}
+                      />
+                    ) : null
+                  }>
+                  {el?.text}
+                  {el?.subtitle ? (
+                    <Text color={'gray.500'}>{el.subtitle}</Text>
+                  ) : null}
+                </Actionsheet.Item>
+              );
+            })}
+          </ActionSheetMenuWrapper>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </View>
   );
 };
 
