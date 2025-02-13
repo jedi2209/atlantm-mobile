996fecf9831e4ad3604a526471170a7145d8cb50
Zavarka Team <support@zavarka.team>
2025-02-13 12:15:20 +0100

    Remove bugsnag

--- src/utils/trace-errors.js
+++ src/utils/trace-errors.js
@@ -1,10 +1,8 @@
-// import Bugsnag from '@bugsnag/react-native';
 import * as Sentry from '@sentry/react-native';
 
 export default class TraceInfo {
   static captureException(error) {
     Sentry.captureException(error);
-    // Bugsnag.notify(new Error(error));
   }
 
   static captureMessage(message) {
